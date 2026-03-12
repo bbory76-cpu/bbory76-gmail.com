eaecadf3d2c9cc429355ce3bfd3ae7f76028191f
bbory76-cpu <bbory76@gmail.com>
2026-03-12 22:20:15 +0900

    Change repository to desktop-tutorial and fix base paths

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bbory76-gmail.com/',
+  base: '/desktop-tutorial/',
   server: {
     host: true
   }
